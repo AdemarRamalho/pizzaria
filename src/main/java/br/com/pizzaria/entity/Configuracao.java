83ffcb10a0bcef2b6b7ab96b22a65a61ea0d3ab3
GabiStz <gabriele.steinmetz1901@gmail.com>
2023-08-18 21:23:51 -0300

    Iniciando Entities

--- src/main/java/br/com/pizzaria/entity/Configuracao.java
+++ src/main/java/br/com/pizzaria/entity/Configuracao.java
@@ -1,4 +1,29 @@
 package br.com.pizzaria.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Tb_confg", schema = "public")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Configuracao {
+    @Column(name = "Valor_Pizza")
+    private BigDecimal ValorPizza;
+
+
+
+
+
+
 }
